72ecdeefeaf893f101a04bcaa87e8ee806c24628
Daniel Tehrani <contact@dantehrani.com>
2024-10-14 22:02:38 +0900

    feat: datadog logging

--- packages/api/src/lib/logger.ts
+++ packages/api/src/lib/logger.ts
@@ -1,9 +1,31 @@
-import winston from 'winston';
+import * as winston from 'winston';
+
+const DATADOG_API_KEY = process.env.DATADOG_API_KEY;
+
+const SERVICE_NAME = 'raylac-api';
+
+const httpTransportOptions = {
+  host: 'http-intake.logs.datadoghq.com',
+  path: `/api/v2/logs?dd-api-key=${DATADOG_API_KEY}&ddsource=nodejs&service=${SERVICE_NAME}`,
+};
+
+const useDatadog = !!DATADOG_API_KEY;
+
+if (useDatadog) {
+  // eslint-disable-next-line no-console
+  console.log('Sending logs to Datadog');
+}
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  transports: [new winston.transports.Console()],
+  exitOnError: false,
+  transports: useDatadog
+    ? [
+        new winston.transports.Http(httpTransportOptions),
+        new winston.transports.Console(),
+      ]
+    : [new winston.transports.Console()],
 });
 
 export default logger;
