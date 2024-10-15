0ddf7587160d6003951dcfb3dd5b18dbbf873e56
Daniel Tehrani <contact@dantehrani.com>
2024-10-15 18:39:34 +0900

    chore: measure query performance

--- packages/api/src/lib/prisma.ts
+++ packages/api/src/lib/prisma.ts
@@ -1,6 +1,26 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+import logger from './logger';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  query: {
+    async $allOperations({ model, operation, args, query }) {
+      const start = Date.now();
+      /* your custom logic for modifying all Prisma Client operations here */
+
+      const result = await query(args);
+
+      const duration = Date.now() - start;
+
+      logger.info(`Query on ${model} took ${duration}ms`, {
+        model,
+        operation,
+        duration,
+      });
+
+      return result;
+    },
+  },
+});
 
 export default prisma;
