4a1b8861c1df54de3eb4a7aaac03dfd30901e78b
Daniel Tehrani <contact@dantehrani.com>
2024-10-16 19:44:44 +0900

    remove: delete outdated tests

--- packages/sync/tests/utils.ts
+++
@@ -1,8 +0,0 @@
-import { createTestClient, http } from 'viem';
-import { anvil } from 'viem/chains';
-
-export const testClient = createTestClient({
-  chain: anvil,
-  mode: 'anvil',
-  transport: http(),
-});
