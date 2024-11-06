5d2ed9a2694887bad218bfc974992ce71a478967
Daniel Tehrani <contact@dantehrani.com>
2024-11-06 14:23:32 +0900

    chore: nit

---
+++ scripts/deploy-anvil.sh
@@ -0,0 +1,2 @@
+cd ../packages/contracts &&
+PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 forge script script/DeployV2.s.sol --fork-url=http://127.0.0.1:8545 --broadcast
