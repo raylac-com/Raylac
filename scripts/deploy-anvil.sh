958822d864eca93a452d56a3143f3ef075ebd861
Daniel Tehrani <contact@dantehrani.com>
2024-11-08 13:29:09 +0900

    remove: no longer used

--- scripts/deploy-anvil.sh
+++
@@ -1,3 +0,0 @@
-cd ../packages/contracts &&
-PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 forge script script/DeployV2.s.sol --fork-url=$ANVIL_RPC_URL --broadcast &&
-PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 forge script script/Paymaster.s.sol --fork-url=$ANVIL_RPC_URL --sig="deposit"  --broadcast
