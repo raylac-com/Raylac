d2b7c7dd842bd58af6e68ab9f36f41b838546471
Daniel Tehrani <contact@dantehrani.com>
2024-11-18 15:15:26 +0900

    chore: cleanup

--- packages/api/src/api/pruneAnvil.ts
+++ packages/api/src/api/pruneAnvil.ts
@@ -5,63 +5,65 @@ import { devChains } from '@raylac/shared';
 const devChainIds = devChains.map(c => c.id);
 
 const pruneAnvil = async () => {
-  await prisma.addressSyncStatus.deleteMany({
-    where: {
-      chainId: {
-        in: devChainIds,
+  await prisma.$transaction(async tx => {
+    await tx.addressSyncStatus.deleteMany({
+      where: {
+        chainId: {
+          in: devChainIds,
+        },
       },
-    },
-  });
+    });
 
-  await prisma.trace.deleteMany({
-    where: {
-      chainId: {
-        in: devChainIds,
+    await tx.trace.deleteMany({
+      where: {
+        chainId: {
+          in: devChainIds,
+        },
       },
-    },
-  });
+    });
 
-  const devChainTxs = await prisma.transaction.findMany({
-    select: {
-      hash: true,
-    },
-    where: {
-      chainId: {
-        in: devChainIds,
+    const devChainTxs = await tx.transaction.findMany({
+      select: {
+        hash: true,
       },
-    },
-  });
+      where: {
+        chainId: {
+          in: devChainIds,
+        },
+      },
+    });
 
-  await prisma.transaction.deleteMany({
-    where: {
-      chainId: {
-        in: devChainIds,
+    await tx.transaction.deleteMany({
+      where: {
+        chainId: {
+          in: devChainIds,
+        },
       },
-    },
-  });
+    });
 
-  await prisma.block.deleteMany({
-    where: {
-      chainId: {
-        in: devChainIds,
+    await tx.block.deleteMany({
+      where: {
+        chainId: {
+          in: devChainIds,
+        },
       },
-    },
-  });
+    });
 
-  await prisma.userOperation.deleteMany({
-    where: {
-      chainId: {
-        in: devChainIds,
+    await tx.userOperation.deleteMany({
+      where: {
+        chainId: {
+          in: devChainIds,
+        },
       },
-    },
-  });
+    });
 
-  await prisma.userAction.deleteMany({
-    where: {
-      txHashes: {
-        hasSome: devChainTxs.map(tx => tx.hash),
+    await tx.userAction.deleteMany({
+      where: {
+        txHashes: {
+          hasSome: devChainTxs.map(tx => tx.hash),
+        },
       },
-    },
+    });
   });
 
   logger.info('Pruned anvil');
--- packages/sync/src/syncUserActions.ts
+++ packages/sync/src/syncUserActions.ts
@@ -1,8 +1,9 @@
 import { Prisma } from '@raylac/db';
 import prisma from './lib/prisma';
 import { Hex } from 'viem';
-import { bigIntMax, decodeUserOperationContext, sleep } from '@raylac/shared';
+import { bigIntMax, decodeUserOperationContext } from '@raylac/shared';
 import { logger } from '@raylac/shared-backend';
+import { loop } from './utils';
 
 /**
  * Get all transactions that haven't been assigned to a `UserAction` yet
@@ -35,61 +36,62 @@ const syncMultiChainTransfers = async ({
 }: {
   chainIds: number[];
 }) => {
-  while (true) {
-    const unassignedTxs = await getUnassignedTxs({ chainIds });
+  loop({
+    fn: async () => {
+      const unassignedTxs = await getUnassignedTxs({ chainIds });
 
-    // Decode all transactions
-    const decodedTxs = unassignedTxs.map(tx => {
-      const { multiChainTag, numChains } = decodeUserOperationContext({
-        txHash: tx.hash as Hex,
-        context: tx.tag as Hex,
+      // Decode all transactions
+      const decodedTxs = unassignedTxs.map(tx => {
+        const { multiChainTag, numChains } = decodeUserOperationContext({
+          txHash: tx.hash as Hex,
+          context: tx.tag as Hex,
+        });
+
+        return { ...tx, multiChainTag, numChains };
       });
 
-      return { ...tx, multiChainTag, numChains };
-    });
+      // Get all unique multi chain tags
+      const multiChainTags = new Set(decodedTxs.map(tx => tx.multiChainTag));
 
-    // Get all unique multi chain tags
-    const multiChainTags = new Set(decodedTxs.map(tx => tx.multiChainTag));
-
-    for (const multiChainTag of multiChainTags) {
-      // Find other transactions with the same multi chain tag
-      const txsInGroup = decodedTxs.filter(
-        tx => tx.multiChainTag === multiChainTag
-      );
-      const numChains = txsInGroup[0].numChains;
-
-      if (txsInGroup.length !== numChains) {
-        logger.info(
-          `Waiting for ${numChains} transactions in group ${multiChainTag}`
+      for (const multiChainTag of multiChainTags) {
+        // Find other transactions with the same multi chain tag
+        const txsInGroup = decodedTxs.filter(
+          tx => tx.multiChainTag === multiChainTag
         );
-        continue;
+        const numChains = txsInGroup[0].numChains;
+
+        if (txsInGroup.length !== numChains) {
+          logger.info(
+            `Waiting for ${numChains} transactions in group ${multiChainTag}`
+          );
+          continue;
+        }
+
+        const txHashes = txsInGroup.map(tx => tx.hash).sort();
+
+        // Create a new UserAction for the group
+        const data: Prisma.UserActionCreateInput = {
+          groupTag: multiChainTag,
+          groupSize: numChains,
+          transactions: {
+            connect: txHashes.map(hash => ({ hash })),
+          },
+          txHashes,
+          // Use the timestamp of the latest transaction as the timestamp of the UserAction
+          timestamp: bigIntMax(
+            txsInGroup.map(tx => BigInt(tx.block.timestamp || 0n))
+          ),
+        };
+
+        await prisma.userAction.upsert({
+          where: { txHashes },
+          update: data,
+          create: data,
+        });
       }
-
-      const txHashes = txsInGroup.map(tx => tx.hash).sort();
-
-      // Create a new UserAction for the group
-      const data: Prisma.UserActionCreateInput = {
-        groupTag: multiChainTag,
-        groupSize: numChains,
-        transactions: {
-          connect: txHashes.map(hash => ({ hash })),
-        },
-        txHashes,
-        // Use the timestamp of the latest transaction as the timestamp of the UserAction
-        timestamp: bigIntMax(
-          txsInGroup.map(tx => BigInt(tx.block.timestamp || 0n))
-        ),
-      };
-
-      await prisma.userAction.upsert({
-        where: { txHashes },
-        update: data,
-        create: data,
-      });
-    }
-
-    await sleep(2000);
-  }
+    },
+    interval: 2000,
+  });
 };
 
 export default syncMultiChainTransfers;
