9f139f803ffedc1ead6fb4e950585d111382e30b
Daniel Tehrani <contact@dantehrani.com>
2024-12-30 22:36:54 +0900

    feat: icon buttons

--- packages/app/src/components/StyledButton/StyledButton.tsx
+++ packages/app/src/components/StyledButton/StyledButton.tsx
@@ -12,9 +12,17 @@ import { hapticOptions } from '@/lib/utils';
 type StyledButtonProps = PressableProps & {
   isLoading?: boolean;
   title: string;
+  variant?: 'primary' | 'outline';
+  icon?: React.ReactNode;
 };
 
-const StyledButton = ({ title, isLoading, ...props }: StyledButtonProps) => {
+const StyledButton = ({
+  title,
+  isLoading,
+  variant = 'primary',
+  icon,
+  ...props
+}: StyledButtonProps) => {
   // Shared value for scale animation
   const scale = useSharedValue(1);
 
@@ -52,20 +60,31 @@ const StyledButton = ({ title, isLoading, ...props }: StyledButtonProps) => {
         style={[
           {
             height: 50,
-            backgroundColor: colors.primary,
+            backgroundColor:
+              variant === 'primary' ? colors.text : colors.background,
+            borderColor:
+              variant === 'primary' ? colors.background : colors.text,
+            borderWidth: variant === 'primary' ? 0 : 1,
             opacity: props.disabled ? 0.5 : 1,
             borderRadius: 32,
+            flexDirection: 'row',
             justifyContent: 'center',
             alignItems: 'center',
+            columnGap: 8,
           },
           animatedStyle,
         ]}
       >
+        {icon}
         {isLoading ? (
           <ActivityIndicator size={24} color="white" />
         ) : (
           <StyledText
-            style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}
+            style={{
+              color: variant === 'primary' ? colors.background : colors.text,
+              fontSize: 16,
+              fontWeight: 'bold',
+            }}
           >
             {title}
           </StyledText>
