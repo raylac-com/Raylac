bffbcd94d8b27bbaac24598dc88ddabeecf5eccc
Daniel Tehrani <contact@dantehrani.com>
2025-01-02 00:19:27 +0900

    fix: ni

--- packages/app/src/components/FeedbackPressable/FeedbackPressable.tsx
+++ packages/app/src/components/FeedbackPressable/FeedbackPressable.tsx
@@ -5,11 +5,11 @@ import { Pressable, PressableProps } from 'react-native';
 const FeedbackPressable = ({ children, ...props }: PressableProps) => {
   return (
     <Pressable
+      {...props}
       onPress={e => {
         ReactNativeHapticFeedback.trigger('impactMedium', hapticOptions);
         props.onPress?.(e);
       }}
-      {...props}
     >
       {children}
     </Pressable>
