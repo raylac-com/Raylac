268567624bf9ff46ebb2f0f43bd7ef5be9cfe9f7
Daniel Tehrani <contact@dantehrani.com>
2025-01-03 23:07:16 +0900

    chore: nit

--- packages/api/src/api/getTokenBalances/getTokenBalances.mock.ts
+++ packages/api/src/api/getTokenBalances/getTokenBalances.mock.ts
@@ -6,6 +6,34 @@ const getTokenBalancesMock = async ({
 }: {
   addresses: Hex[];
 }): Promise<TokenBalancesReturnType> => {
+  /*
+  return [
+    {
+      address: '0x0000000000000000000000000000000000000000',
+      chainId: 1,
+      token: ETH,
+      balance: {
+        amount: parseUnits('1', ETH.decimals).toString(),
+        formatted: '1',
+        usdValue: '1234.23',
+        usdValueFormatted: '1234.23',
+        tokenPriceUsd: 1234.23,
+      },
+    },
+    {
+      address: '0x0000000000000000000000000000000000000000',
+      chainId: 1,
+      token: USDC,
+      balance: {
+        amount: parseUnits('1234.23', USDC.decimals).toString(),
+        formatted: '1234.23',
+        usdValue: '1234.23',
+        usdValueFormatted: '1234.23',
+        tokenPriceUsd: 1,
+      },
+    },
+  ];
+  */
   return [];
 };
 
