177e429ed8215a678b93b54e388ac495cf34105f
Daniel Tehrani <contact@dantehrani.com>
2025-01-03 15:45:05 +0900

    feat: cleanup settings page

--- packages/app/src/screens/Settings/Settings.tsx
+++ packages/app/src/screens/Settings/Settings.tsx
@@ -1,6 +1,29 @@
+import Entypo from '@expo/vector-icons/Entypo';
+import FeedbackPressable from '@/components/FeedbackPressable/FeedbackPressable';
 import StyledText from '@/components/StyledText/StyledText';
 import useTypedNavigation from '@/hooks/useTypedNavigation';
-import { Pressable, View } from 'react-native';
+import { View } from 'react-native';
+import colors from '@/lib/styles/colors';
+
+const SettingsListItem = ({
+  title,
+  icon,
+  onPress,
+}: {
+  title: string;
+  icon: React.ReactNode;
+  onPress: () => void;
+}) => {
+  return (
+    <FeedbackPressable
+      onPress={onPress}
+      style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}
+    >
+      {icon}
+      <StyledText>{title}</StyledText>
+    </FeedbackPressable>
+  );
+};
 
 const Settings = () => {
   const navigation = useTypedNavigation();
@@ -22,13 +45,17 @@ const Settings = () => {
         padding: 16,
       }}
     >
-      <View style={{ flexDirection: 'column', gap: 16 }}>
-        <Pressable onPress={onAddressesPress}>
-          <StyledText>{`Addresses`}</StyledText>
-        </Pressable>
-        <Pressable onPress={onAdvancedPress}>
-          <StyledText>{`Advanced`}</StyledText>
-        </Pressable>
+      <View style={{ flexDirection: 'column', gap: 28 }}>
+        <SettingsListItem
+          title="Addresses"
+          icon={<Entypo name="wallet" size={24} color={colors.border} />}
+          onPress={onAddressesPress}
+        />
+        <SettingsListItem
+          title="Advanced"
+          icon={<Entypo name="rocket" size={24} color={colors.border} />}
+          onPress={onAdvancedPress}
+        />
       </View>
     </View>
   );
