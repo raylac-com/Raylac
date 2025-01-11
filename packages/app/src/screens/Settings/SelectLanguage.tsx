9a9696437c3b5701f87ee4e378fa1399ab3581d7
Daniel Tehrani <contact@dantehrani.com>
2025-01-11 12:51:15 +0900

    Refactor language selection in settings screen
    - Introduced a new `LanguageListItem` component for better code organization and reusability.
    - Updated the `SelectLanguage` component to utilize `LanguageListItem` for displaying language options.
    - Removed unnecessary translation function calls for cleaner code.
    
    Co-Authored-By: Daniel Tehrani <daniel.tehrani@raylac.com>

--- packages/app/src/screens/Settings/SelectLanguage.tsx
+++ packages/app/src/screens/Settings/SelectLanguage.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { Feather } from '@expo/vector-icons';
 import { View } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { saveSelectedLanguage } from '@/i18n';
 import FeedbackPressable from '@/components/FeedbackPressable/FeedbackPressable';
 import StyledText from '@/components/StyledText/StyledText';
 
+const LanguageListItem = ({
+  isSelected,
+  language,
+  onPress,
+}: {
+  isSelected: boolean;
+  language: string;
+  onPress: () => void;
+}) => {
+  return (
+    <FeedbackPressable onPress={onPress}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}>
+        <View
+          style={{
+            width: 24,
+            height: 24,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {isSelected && <Feather name="check" size={18} color="black" />}
+        </View>
+        <StyledText>{language}</StyledText>
+      </View>
+    </FeedbackPressable>
+  );
+};
+
 const SelectLanguage = () => {
-  const { i18n, t } = useTranslation();
+  const { i18n } = useTranslation();
 
   const handleLanguageChange = async (lang: 'en' | 'ja') => {
     saveSelectedLanguage(lang);
@@ -22,26 +52,16 @@ const SelectLanguage = () => {
         gap: 16,
       }}
     >
-      <FeedbackPressable
+      <LanguageListItem
+        isSelected={i18n.language === 'en'}
+        language="English"
         onPress={() => handleLanguageChange('en')}
-        style={{
-          padding: 12,
-          borderRadius: 8,
-          backgroundColor: 'white',
-        }}
-      >
-        <StyledText>{t('SelectLanguage.english')}</StyledText>
-      </FeedbackPressable>
-      <FeedbackPressable
+      />
+      <LanguageListItem
+        isSelected={i18n.language === 'ja'}
+        language="日本語"
         onPress={() => handleLanguageChange('ja')}
-        style={{
-          padding: 12,
-          borderRadius: 8,
-          backgroundColor: 'white',
-        }}
-      >
-        <StyledText>{t('SelectLanguage.japanese')}</StyledText>
-      </FeedbackPressable>
+      />
     </View>
   );
 };
