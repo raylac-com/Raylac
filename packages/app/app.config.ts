46f56dc9d54134b345737586664fd66b02862a47
Daniel Tehrani <contact@dantehrani.com>
2025-01-12 15:03:21 +0900

    chore: use separate runtime for android and ios

--- packages/app/app.config.ts
+++ packages/app/app.config.ts
@@ -49,6 +49,7 @@ const config: ExpoConfig = {
       usesNonExemptEncryption: false,
     },
     bundleIdentifier,
+    runtimeVersion: '1.2.0',
   },
   android: {
     adaptiveIcon: {
@@ -56,6 +57,7 @@ const config: ExpoConfig = {
       backgroundColor: '#ffffff',
     },
     package: bundleIdentifier,
+    runtimeVersion: '1.3.0',
   },
   plugins: [
     [
@@ -90,7 +92,6 @@ const config: ExpoConfig = {
   updates: {
     url: 'https://u.expo.dev/f1bcc97f-3192-41c1-a32e-5b1dd12049cd',
   },
-  runtimeVersion: '1.3.0',
   owner: 'raylac',
 };
 
